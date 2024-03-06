f594f9d6ecf793fc61c518a874d915391b331e32
LeandrSDEV <133284910+LeandrSDEV@users.noreply.github.com>
2024-03-06 16:50:33 -0300

    Add files via upload

---
+++ h12/h12/Entities/OutsourceEmployee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace h12.Entities
+{
+    internal class OutsourceEmployee : Employee
+    {
+        public double additionalCharge { get; set; }
+
+        public OutsourceEmployee() { }
+
+        public OutsourceEmployee(string name, int hours, double valuePerHour, double additionalCharge) :base(name, hours, valuePerHour)
+        {
+            this.additionalCharge = additionalCharge;
+        }
+
+        public override double Payment()
+        {
+            return base.Payment() + 1.1 * additionalCharge;
+        }
+    }
+}
